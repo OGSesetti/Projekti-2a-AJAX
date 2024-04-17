a9d8fa51a43a9301cdee19e37f4431875530e8ea
OGSesetti <91277863+OGSesetti@users.noreply.github.com>
2024-04-17 20:42:07 +0300

    ukjh

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+/*
+Käytä linkkejä:
+http://www.finnkino.fi/xmlLinks to an external site.
+http://www.finnkino.fi/xml/Schedule/
+http://www.omdbapi.com/Links to an external site. (kuvat?)
+https://github.com/public-apis/public-apis?tab=readme-ov-file#anime (saattaa olla hyödyllinen)
 
+Nettisivu, napit muutamalle leffateatterille, pyydä niillä ehdoilla leffan nimi, kuvat?, ja
+näytösaika.
+
+Ota selvää voiko kuvat tehdä jotenkin hienosti. (vissiin voi)
